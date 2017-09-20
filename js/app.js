fe07faedf1e0121b47153d3da574ed4edfbe4320
Samuel Cleophas <samuel.skater@gmail.com>
2017-09-20 14:50:09 +1000

    Sprint 43 | Interactive Transcript
    - Created event handler to highlight sentences based on currentTime
    - Created event handler to set video time based on clicks to sentences

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const video = document.querySelector('video');
+const transcriptDiv = document.querySelector('.transcript');
+
+// Highlight Transcript based on time-stamps
+video.addEventListener('timeupdate', () => {
+  sentences = document.querySelectorAll('.transcript span');
+  currentTime = video.currentTime;
+  
+  for (let i = 0; i < sentences.length; i++) {
+    let sentence = sentences[i];
+    
+    if (sentence.nextElementSibling) {
+      const nextSentence = sentences[i].nextElementSibling;
+      const sentenceEnd = parseFloat(nextSentence.getAttribute('data-time'));
+      let sentenceStart = parseFloat(sentence.getAttribute('data-time'));
+
+      if (sentenceStart <= currentTime && currentTime <= sentenceEnd) {
+        sentence.className = "highlight";
+      } else {
+        sentence.className = "";
+      }
+    }
+  }
+});
+
+transcriptDiv.addEventListener('click', (e) => {
+  let sentence = e.target;
+  let time = parseFloat(sentence.getAttribute('data-time'));
+  
+  video.currentTime = time;
+  video.play();
+});
